15da8e9e47ccec1dbbe18ca85880d7631b8d5d97
cyp <2087507579@qq.com>
2017-05-03 17:36:02 +0800

    WebSite

---
+++ Content/js/public.js
@@ -0,0 +1,22 @@
+﻿window.onscroll = function () {
+    console.log(document.body.scrollTop)
+    var st = document.body.scrollTop;
+    var oNav = document.getElementById('head_nav');
+    if (st > 80) {
+        oNav.style.position = 'fixed';
+        oNav.style.top = '0';
+        oNav.style.left = '0';
+        oNav.style.zIndex = 1;
+
+    } else {
+        oNav.style.position = 'fixed';
+        oNav.style.top = '-100px';
+        oNav.style.left = '0';
+        oNav.style.zIndex = -1;
+    }
+}
+
+
+
+
+
