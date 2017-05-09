0953ae1e29a9e03347d50bdb8d75ab6b1b7351ff
cyp <2087507579@qq.com>
2017-05-09 20:50:42 +0800

    顶部导航更改为淡出效果，移动端的适配调整

--- Content/js/public.js
+++ Content/js/public.js
@@ -1,20 +1,8 @@
-﻿window.onscroll = function () {
-    console.log(document.body.scrollTop)
-    var st = document.body.scrollTop;
-    var oNav = document.getElementById('head_nav');
-    if (st > 80) {
-        oNav.style.position = 'fixed';
-        oNav.style.top = '0';
-        oNav.style.left = '0';
-        oNav.style.zIndex = 1;
-
-    } else {
-        oNav.style.position = 'fixed';
-        oNav.style.top = '-100px';
-        oNav.style.left = '0';
-        oNav.style.zIndex = -1;
-    }
-}
+﻿$(window).scroll(function () {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav");
+    } else { $(".navbar-fixed-top").removeClass("top-nav"); }
+})
 
 
 
