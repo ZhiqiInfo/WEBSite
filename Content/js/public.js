1992079d1793b3e4a8a07cd747be5c508475ff32
cyp <2087507579@qq.com>
2017-05-13 12:00:22 +0800

    20170513数据填充

--- Content/js/public.js
+++ Content/js/public.js
@@ -4,7 +4,18 @@
     } else { $(".navbar-fixed-top").removeClass("top-nav"); }
 })
 
+$(function () {
+    $("#Flexible").click(function () {
+        if ($(".fcn_list .fcn_listdiv").size() >= 3) {
+            var html = "";
+            $.each($('.fcn_list .fcn_listdiv:gt(3)'), function () {
+                $(this).toggle();
+            })
+        }
+    })
+   
 
+})
 
 
 
